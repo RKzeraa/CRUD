960ed83d2ab1cd52102478c2031fca87444dd002
RKzeraa <rkzeraa@gmail.com>
2023-05-09 08:34:53 -0300

    [Fix] - Was edited scope IsValidEmail

--- userCRUD/Controller/UserController.cs
+++ userCRUD/Controller/UserController.cs
@@ -218,7 +218,7 @@ internal class UserController : IUserController
         return true;
     }
 
-    static bool IsValidEmail(string email)
+    private static bool IsValidEmail(string email)
     {
         Regex emailRx = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase);
         return emailRx.IsMatch(email);
